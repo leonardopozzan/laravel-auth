58322f489bcc1baf6b02706c97a45e5c50f72f65
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-12 16:47:39 +0100

    inserito relazioni nei model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -14,5 +15,8 @@ class Project extends Model
 
     public static function generateSlug($name){
         return Str::slug($name, '-');
+    }
+    public function type(): BelongsTo{
+        return $this->belongTo(Type::class);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 
@@ -16,4 +17,9 @@ class Type extends Model
     public static function generateSlug($name){
         return Str::slug($name, '-');
     }
+
+    public function projects(): HasMany{
+        return $this->hasMany(Project::class);
+    }
+
 }
